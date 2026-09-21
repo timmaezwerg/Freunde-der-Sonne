a5863ae413177808b87559adb33e296cbee414b6
timmaezwerg <timmaezwerg@googlemail.com>
2026-09-21 16:58:13 +0200

    Umbruch

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Freunde der Sonne - Service Worker with Offline Cache & Push
    ========================================================= */
 
-const CACHE_NAME = 'fds-cache-v20260921-2';
+const CACHE_NAME = 'fds-cache-v20260921-3';
 const PRECACHE_ASSETS = [
   './',
   './index.html',
